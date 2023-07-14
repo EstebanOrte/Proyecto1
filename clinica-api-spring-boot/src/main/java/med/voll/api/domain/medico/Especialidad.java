c00a8ea94f1668c9f7868773f99a209be608f8b7
EstebanOrte <122826035+EstebanOrte@users.noreply.github.com>
2023-07-13 20:04:53 -0500

    clinica-api-spring-boot

---
+++ clinica-api-spring-boot/src/main/java/med/voll/api/domain/medico/Especialidad.java
@@ -0,0 +1,12 @@
+package med.voll.api.domain.medico;
+
+public enum Especialidad {
+Medicina_general,
+Cardiología,
+Medicina_interna,
+Dermatología,
+Rehabilitación_física,
+Psicología,
+Odontología,
+Radiología,
+}
